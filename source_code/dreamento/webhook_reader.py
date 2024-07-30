83061f2041fa621775266b7e1bc10a5981b71fde
David Kreisler <a01569177@unet.univie.ac.at>
2024-07-30 16:09:23 +0200

    example webhook reader implemented

---
+++ source_code/dreamento/webhook_reader.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhookcallback', methods=['POST'])
+def hook():
+    print(request.data)
+    return "received"
+
+
+if __name__ == '__main__':
+    app.run()
